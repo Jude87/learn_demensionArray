3d96c39c45f2fa51cd132b79414b738b917e238c
Jude87 <107044598+Jude87@users.noreply.github.com>
2022-07-11 23:00:26 +0900

    Add files via upload

---
+++ RUN.java
@@ -0,0 +1,11 @@
+package practice.demensionArray;
+
+public class RUN {
+
+	public static void main(String[] args) {
+		DemensionArrayPractice dp = new DemensionArrayPractice();
+		//dp.method1();
+		dp.method2();
+	}
+
+}
